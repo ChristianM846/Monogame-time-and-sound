f95e7f6d82adede7eca550253525c8d426a0ade7
Christian Moyes <moyechri846@gmail.com>
2023-11-13 10:56:37 -0500

    Good

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Monogame_time_and_sound
 {
@@ -11,6 +13,11 @@ namespace Monogame_time_and_sound
         Texture2D bombTexture;
         Rectangle bombRect;
         SpriteFont timeFont;
+        float seconds;
+        float startTime;
+        MouseState mouseState;
+        SoundEffect explode;
+        bool exploded;
 
         public Game1()
         {
@@ -20,6 +27,7 @@ namespace Monogame_time_and_sound
             graphics.PreferredBackBufferWidth = 800;
             graphics.PreferredBackBufferHeight = 500;
             graphics.ApplyChanges();
+            exploded = false;
         }
 
         protected override void Initialize()
@@ -32,14 +40,31 @@ namespace Monogame_time_and_sound
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             bombTexture = Content.Load<Texture2D>("bomb");
+            timeFont = Content.Load<SpriteFont>("BombTimer");
+            explode = Content.Load<SoundEffect>("explosion");
 
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+
+            //if (mouseState.LeftButton == ButtonState.Pressed)
+            //{
+            //    startTime = (float)gameTime.TotalGameTime.TotalSeconds;
+            //}
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            seconds = (float)gameTime.TotalGameTime.TotalSeconds - startTime;
+
+            if (Math.Round(seconds) == 15 && !exploded) 
+            {
+                explode.Play();
+                //startTime = (float)gameTime.TotalGameTime.TotalSeconds;
+                exploded = true;
+            }
 
             base.Update(gameTime);
         }
@@ -50,6 +75,14 @@ namespace Monogame_time_and_sound
             spriteBatch.Begin();
             spriteBatch.Draw(bombTexture, bombRect, Color.White);
 
+            if (seconds < 15)
+            {
+                spriteBatch.DrawString(timeFont, (15 - seconds).ToString("00.0"), new Vector2(270, 200), Color.Black);
+            }
+            else if (seconds >= 15)
+            {
+                spriteBatch.DrawString(timeFont, "Boom", new Vector2(246, 210), Color.Black);
+            }
 
             spriteBatch.End();
 
